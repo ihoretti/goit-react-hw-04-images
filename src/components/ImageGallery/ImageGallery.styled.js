9c0b889646c6bbf5d5b26508536fd25d909a4d4f
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-12-20 00:17:26 +0200

    Create ImageGallery.styled.js

---
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const ImgList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  list-style: none;
+`;
+
+export default ImgList;
+
+// display: grid;
+// max-width: calc(100vw - 48px);
+// grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+// grid-gap: 26px;
+// margin-top: 0;
+// margin-bottom: 0;
+// padding: 0;
+// list-style: none;
+// margin-left: auto;
+// margin-right: auto;
