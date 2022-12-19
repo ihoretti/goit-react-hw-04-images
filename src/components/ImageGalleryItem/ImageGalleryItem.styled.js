025f249b29b8726d6e699f8f92139cf6d3adf08b
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-12-20 00:17:36 +0200

    Create ImageGalleryItem.styled.js

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Items = styled.li`
+  margin-right: 10px;
+  margin-bottom: 10px;
+  margin-top: 0;
+  :hover {
+    box-shadow: 0px 10px 10px -3px rgba(0, 0, 0, 0.6);
+    cursor: pointer;
+  }
+`;
+
+export const Img = styled.img`
+  display: block;
+  width: 300px;
+  height: 200px;
+`;
