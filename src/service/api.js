40eb57b0371835fb31bb2eb4ee8dc0b9249b736e
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-12-21 23:26:59 +0200

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -4,10 +4,23 @@ const KEY = '30661779-0ac9a2888848bc4b079840e05';
 const URL = 'https://pixabay.com/api/';
 
 const getImages = async (inputValue, page) => {
-  return await axios.get(
+  const { data } = await axios.get(
     `${URL}?q=${inputValue}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
+
+  const images = getNormalizedImages(data.hits);
+  const totalPages = Math.ceil(data.totalHits / 12);
+
+  return { images, totalPages };
 };
+
+const getNormalizedImages = imagesData =>
+  imagesData.map(({ id, webformatURL, largeImageURL, tags }) => ({
+    id,
+    webformatURL,
+    largeImageURL,
+    tags,
+  }));
 
 //return await axios.get(`${URL}?q=${valueInput}&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
 
